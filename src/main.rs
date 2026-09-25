1b6a20cfd2f0fe8176f5b4329a839bf55bc5a1dd
hakilebara <frederic.soumare@gmail.com>
2026-09-25 16:06:23 +0200

    Create a stub database, write and read a row from it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{params, Connection};
+use uuid::Uuid;
+
+fn main() -> anyhow::Result<()> {
+    let conn = Connection::open_in_memory()?;
+    let mut stmt =
+        conn.prepare("CREATE TABLE blocks (id TEXT PRIMARY KEY, title TEXT NOT NULL)")?;
+    stmt.execute([])?;
+
+    let uuid = Uuid::new_v4();
+    stmt = conn.prepare("INSERT INTO blocks (id, title) VALUES (?1, ?2)")?;
+    stmt.execute(params![uuid.to_string(), "hello"])?;
+
+    let title: String = conn.query_one(
+        "SELECT id, title FROM blocks WHERE title == 'hello'",
+        [],
+        |row| row.get(1),
+    )?;
+    println!("{}", title);
+    Ok(())
 }
